70f44c1762125f6de4b123a25c75f47822c04f8c
Florian Zschocke <florian.zschocke@devolo.de>
2021-07-30 16:28:23 +0200

    Create initial chain with a first block.
    We start small with a 100 Euro block, so we can grow big!

--- bling.c
+++ bling.c
@@ -2,11 +2,16 @@
  * The ultimate blockchain program
  */
 
+#include <stddef.h>
+
 #include "blocknicator/include/blocknicator.h"
 
 
 int main (int argc, char** argv )
 {
+	void* block = create_block("100€");
+	void* chain = chain_block(block, NULL);
+
 	return 0;
 }
 
